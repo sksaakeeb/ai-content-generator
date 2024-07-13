ee8a29c1b7f1faefdb5f644c96639077a992584b
Sk Sakib <sksakib066@gmail.com>
2024-07-13 20:17:52 +0530

    Initial commit

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,8 +1,14 @@
+import { Search } from 'lucide-react'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='p-7'>
+      <div className='flex gap-2 item-center p-2 border rounded-md max-w-md flex item-center'>
+        <Search />
+        <input type="text" placeholder='Search...' className='outline-none'/>
+      </div>
+    </div>
   )
 }
 
